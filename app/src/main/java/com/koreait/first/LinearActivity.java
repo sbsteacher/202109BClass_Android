e488645f7cd497c5953bdeb1354d92ba98b51d8d
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-10-15 09:46:16 +0900

    2021-10-15 1교시

--- app/src/main/java/com/koreait/first/LinearActivity.java
+++ app/src/main/java/com/koreait/first/LinearActivity.java
@@ -9,6 +9,7 @@ public class LinearActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_linear);
+        //setContentView(R.layout.activity_linear);
+        setContentView(R.layout.activity_linear_exam1);
     }
 }
