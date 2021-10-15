d1e35cc53c4cc0d8b0ce2de7b41f296bbe86cd5a
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-10-15 16:36:18 +0900

    2021-10-15 마무리

---
+++ app/src/main/java/com/koreait/first/MenuActivity.java
@@ -0,0 +1,64 @@
+package com.koreait.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void call(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW
+                , Uri.parse("tel:"));
+        startActivity(intent);
+    }
+
+    public void moveToActivity(View v) {
+        int id = v.getId();
+
+        Class c = null;
+        if(id == R.id.menuBtn1) {
+            c = MainActivity.class;
+        } else if(id == R.id.menuBtn2) {
+            c = LinearActivity.class;
+        } else if(id == R.id.menuBtn3) {
+            c = ConstraintActivity.class;
+        }
+
+        Intent intent = new Intent(this, c);
+        startActivity(intent);
+    }
+
+    public void moveToActivityWithText(View v) {
+        TextView tv = (TextView)v;
+        String text = (String)tv.getText();
+        Log.i("myLog", text);
+
+        Class c = null;
+        switch(text) {
+            case "메인":
+                c = MainActivity.class;
+                break;
+            case "리니어레이아웃":
+                c = LinearActivity.class;
+                break;
+            case "제약레이아웃":
+                c = ConstraintActivity.class;
+                break;
+        }
+
+        Intent intent = new Intent(this, c);
+        startActivity(intent);
+    }
+
+}
