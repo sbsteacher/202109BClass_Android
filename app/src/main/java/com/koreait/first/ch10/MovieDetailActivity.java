5bf199fdcfb9d7b57914e7061924ac46b76cbe71
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-10-25 16:18:00 +0900

    2021-10-25 5교시

--- app/src/main/java/com/koreait/first/ch10/MovieDetailActivity.java
+++ app/src/main/java/com/koreait/first/ch10/MovieDetailActivity.java
@@ -5,13 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.koreait.first.R;
+import com.koreait.first.ch10.searchmoviemodel.MovieInfoResultBodyVO;
+import com.koreait.first.ch10.searchmoviemodel.MovieInfoResultVO;
+import com.koreait.first.ch10.searchmoviemodel.MovieInfoVO;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MovieDetailActivity extends AppCompatActivity {
+
+    private TextView tvMovieNm;
+    private TextView tvMovieNmEn;
+    private TextView tvShowTm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +31,11 @@ public class MovieDetailActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String movieCd = intent.getStringExtra("movieCd");
         Log.i("myLog", "detail - movieCd : " + movieCd);
-
         getData(movieCd);
+
+        tvMovieNm = findViewById(R.id.tvMovieNm);
+        tvMovieNmEn = findViewById(R.id.tvMovieNmEn);
+        tvShowTm = findViewById(R.id.tvShowTm);
     }
 
     private void getData(String movieCd) {
@@ -32,6 +46,38 @@ public class MovieDetailActivity extends AppCompatActivity {
 
         KobisService service = rf.create(KobisService.class);
         final String KEY = "1a0a7ecf96ad3364d8de70e91560767a";
-        service.searchMovieInfo(KEY, movieCd);
+        Call<MovieInfoResultBodyVO> call = service.searchMovieInfo(KEY, movieCd);
+        call.enqueue(new Callback<MovieInfoResultBodyVO>() {
+            @Override
+            public void onResponse(Call<MovieInfoResultBodyVO> call, Response<MovieInfoResultBodyVO> res) {
+                if(res.isSuccessful()) { //state : 200 - 통신 성공!!!
+                    MovieInfoResultBodyVO result = res.body(); //
+
+                    //2줄로 풀어쓰기
+                    MovieInfoResultVO result2 = result.getMovieInfoResult();
+                    MovieInfoVO data = result2.getMovieInfo();
+
+                    //1줄로 쓰기
+                    MovieInfoVO data2 = result.getMovieInfoResult().getMovieInfo();
+
+                    //전부 1줄
+                    MovieInfoVO data3 = res.body().getMovieInfoResult().getMovieInfo();
+
+                    //data나 data2나 같은 주소값
+
+                    tvMovieNm.setText(data.getMovieNm());
+                    tvMovieNmEn.setText(data.getMovieNmEn());
+                    tvShowTm.setText(data.getShowTm());
+
+                } else {
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MovieInfoResultBodyVO> call, Throwable t) { //baseUrl이 틀리거나, 그 서버가 죽어있을 때, 망에 문제 있거나
+
+            }
+        });
     }
 }
